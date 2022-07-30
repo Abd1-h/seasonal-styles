f9b98f4ae6a1c54ca408060fce135ed43c9ee723
Abd1-h <Abdi.hussein6121@gmail.com>
2022-07-29 21:27:36 -0700

    update

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,5 +3,29 @@ $("document").ready(function(){
         e.preventDefault();//stop default submission
         var season = $(this).attr("href");//contents of href attribute of this element
         alert(season);
+
+        $("html").css("background-color","#2B7129");
+        $("#wear").attr("src", "images/spring-wear.jpg");
+        $("#logo").attr("src", "images/spring.gif");
+        $("header h3").text("Jump Into Spring Wear!");
+
+        $("html").css("background-color","#EBA52B");
+        $("#wear").attr("src", "images/summer-wear.jpg");
+        $("#logo").attr("src", "images/summer.gif");
+        $("header h3").text("Jump Into Summer Wear!");
+
+        $("html").css("background-color","#A81124");
+        $("#wear").attr("src", "images/fall-wear.jpg");
+        $("#logo").attr("src", "images/fall.gif");
+        $("header h3").text("Jump Into Fall Wear!");
+
+        $("html").css("background-color","#005393");
+        $("#wear").attr("src", "images/winter-wear.jpg");
+        $("#logo").attr("src", "images/winter.gif");
+        $("header h3").text("Jump Into Winter Wear!");
     });
+
+   
+
+
 });
